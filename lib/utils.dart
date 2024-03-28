814d907f8b75aaa086030553d1415a341535c742
Guillaume Digier <guillaume.digier@he-arc.ch>
2024-03-28 09:39:22 +0100

    little thing

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Utils {
-  /// A method to build a widget based on the state of a future's snapshot.
-  static Widget futureBuilding<T>({
+  /// A method to build a widget based on the state of a stream's snapshot.
+  static Widget streamBuilding<T>({
     required AsyncSnapshot<T> snap,
     required Widget Function(T data) builder,
   }) {
     if (!snap.hasError) {
-      if (snap.connectionState == ConnectionState.done) {
+      if (snap.connectionState == ConnectionState.active) {
         if (snap.hasData) {
+          // If snapshot has no error, is active, and has data, widget is built.
           return builder(snap.data as T);
         } else {
           return const Center(child: Text('No data'));
@@ -21,13 +22,13 @@ class Utils {
     }
   }
 
-  /// A method to build a widget based on the state of a stream's snapshot.
-  static Widget streamBuilding<T>({
+  /// A method to build a widget based on the state of a future's snapshot.
+  static Widget futureBuilding<T>({
     required AsyncSnapshot<T> snap,
     required Widget Function(T data) builder,
   }) {
     if (!snap.hasError) {
-      if (snap.connectionState == ConnectionState.active) {
+      if (snap.connectionState == ConnectionState.done) {
         if (snap.hasData) {
           return builder(snap.data as T);
         } else {
